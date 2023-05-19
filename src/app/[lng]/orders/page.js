8abeed10f3954a13cb4231400bbfa2b46b3f7458
Viet Anh Nguyen <vietanh.dev@gmail.com>
2023-05-20 00:39:21 +0700

    Show orders for customers

--- src/app/[lng]/orders/page.js
+++ src/app/[lng]/orders/page.js
@@ -1,47 +1,19 @@
 "use client";
-import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import {
-  calculatePriceForList,
-  getItemsWithPrice,
-  calculateTax,
-  calculateTotalPriceWithTax,
-} from "../../../lib/price";
 import { useTranslation } from "../../i18n/client";
-import { PlaceOrder } from "@/lib/requests/orders";
+import { GetOrders } from "@/lib/requests/orders";
 
 export default function Cart({ params: { lng } }) {
-  const router = useRouter();
-  const [totalPrice, setTotalPrice] = useState(0);
-  const [itemsWithPrice, setItemsWithPrice] = useState([]);
-
-  function updateTotalPrice() {
-    const cart = JSON.parse(localStorage.getItem("cart", "[]"));
-    let price = calculatePriceForList(cart);
-    setTotalPrice(price);
-  }
-
-  function getItemsWithPriceFromLocalStorage() {
-    const cart = JSON.parse(localStorage.getItem("cart", "[]"));
-    let items = getItemsWithPrice(cart);
-    setItemsWithPrice(items);
-    updateTotalPrice();
-  }
+  const [orderItems, setOrderItems] = useState([]);
 
   useEffect(() => {
-    getItemsWithPriceFromLocalStorage();
+    GetOrders((orders) => {
+      setOrderItems(orders);
+    }, (e) => {
+      console.log(e);
+      alert("Could not get orders")
+    })
   }, []);
-
-  function handlePlaceOrder() {
-    const cart = JSON.parse(localStorage.getItem("cart", "[]"));
-    PlaceOrder(
-      cart,
-      (onSuccess = () => {
-        localStorage.setItem("cart", "[]");
-        router.push(`/${lng}/orders`);
-      })
-    );
-  }
 
   const { t } = useTranslation(lng, "common");
   return (
@@ -53,69 +25,34 @@ export default function Cart({ params: { lng } }) {
             <span>X</span>
           </a>
         </p>
-        {itemsWithPrice.map((item, itemId) => {
+        {orderItems.map((order, orderId) => {
+          const status = order.status;
+          const price = order.price;
+          const tax = order.tax;
+          const totalPrice = order.totalPrice;
+          const items = order.items;
+          const timestamp = order.timestamp;
+          const orderTime = new Date(timestamp).toLocaleString();
+          const itemsWithPrice = order.items;
+
           return (
             <div
-              key={itemId}
+              key={orderId}
               className="flex flex-col items-center justify-center w-full p-4 min-h-[100px] my-1 mx-1 border-b-2"
             >
               <div className="flex flex-col items-begin justify-center w-full relative">
-                <p className="text-xl font-bold">{t(item.name)}</p>
-                <p className="text-sm">{t(item.description)}</p>
-                {item.customizations.map(
-                  (customization, customizationIndex) => {
-                    return (
-                      <p key={customizationIndex} className="text-sm">
-                        {t(customization.name)}:{" "}
-                        {t(
-                          customization.options[
-                            item.selectedOptions[customizationIndex]
-                          ]?.name
-                        )}
-                      </p>
-                    );
-                  }
-                )}
+                <p className="text-sm">{t("Status")}: {status}</p>
+                <p className="text-sm">{orderTime}</p>
                 <p className="text-sm">
-                  {t("Quantity")}: {item.quantity}
+                  {t("Number of items")}: {itemsWithPrice.length}
                 </p>
                 <p className="absolute right-0 top-0 text-sm float-right">
-                  ${item.price}
+                  ${totalPrice.toFixed(2)}
                 </p>
-                {/* Remove button */}
-                <button
-                  className="absolute right-0 bottom-0 text-md text-gray-500 float-right"
-                  onClick={() => {
-                    let cart = JSON.parse(localStorage.getItem("cart", "[]"));
-                    cart.splice(itemId, 1);
-                    localStorage.setItem("cart", JSON.stringify(cart));
-                    setItemsWithPrice(getItemsWithPrice(cart));
-                    updateTotalPrice();
-                  }}
-                >
-                  {t("Remove")}
-                </button>
               </div>
             </div>
           );
         })}
-        <div className="flex flex-col items-center justify-center w-full p-4 min-h-[100px] my-1 mx-1 border-b-2">
-          <div className="flex flex-col items-begin justify-center w-full relative">
-            <p className="text-xl font-bold">{t("Tax")}</p>
-            <p className="absolute right-0 top-0 text-sm float-right">
-              ${calculateTax(totalPrice).toFixed(2)}
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div className="w-full max-w-[700px] fixed bottom-0 md:bottom-2 h-[50px] border-t-[1px] md:border-[1px] border-gray-600 p-2 bg-[#A3DE69] md:rounded-md">
-        <span className="text-2xl" onClick={handlePlaceOrder}>
-          {t("Place Order!")}
-        </span>
-        <span className="text-2xl float-right">
-          ${calculateTotalPriceWithTax(totalPrice).toFixed(2)}
-        </span>
       </div>
     </main>
   );
--- src/lib/requests/orders.js
+++ src/lib/requests/orders.js
@@ -1,11 +1,31 @@
 export async function PlaceOrder(items, onSuccess = null, onError = null) {
-  // Call the API to place the order
   const response = await fetch("/api/customers/orders", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(items),
+  });
+
+  const data = await response.json();
+  if (data?.success) {
+    if (onSuccess) {
+      onSuccess(data?.data);
+    }
+  } else {
+    if (onError) {
+      onError(data);
+    }
+  }
+}
+
+
+export async function GetOrders(onSuccess = null, onError = null) {
+  const response = await fetch("/api/customers/orders", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
   });
 
   const data = await response.json();
