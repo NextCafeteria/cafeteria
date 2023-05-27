4c424b1354f7f8728c3d1b31ec491d170e59c645
sonthb3992 <sonthb@avnaudit.vn>
2023-05-27 09:40:01 +0700

    Update formatting

--- src/lib/price.js
+++ src/lib/price.js
@@ -37,7 +37,7 @@ export function calculatePrice(id, selectedOptions, quantity = 1) {
 
 export function calculatePriceForList(items) {
   let totalPrice = 0;
-  if (items=== null) return 0;
+  if (items === null) return 0;
   items.forEach((item) => {
     totalPrice += calculatePrice(item.id, item.selectedOptions, item.quantity);
   });
