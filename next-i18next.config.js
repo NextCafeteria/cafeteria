894361a6aa6fe14cf72b9be7f2dffef1c09606c1
Viet Anh Nguyen <vietanh.dev@gmail.com>
2023-07-05 19:44:56 +0700

    Default to vi

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   i18n: {
-    defaultLocale: "en",
-    locales: ["en", "vi"],
+    defaultLocale: "vi",
+    locales: ["vi", "en"],
   },
 };
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 "use client";
 import { redirect } from "next/navigation";
 export default async function Home() {
-  redirect("/en");
+  redirect("/vi");
 }
