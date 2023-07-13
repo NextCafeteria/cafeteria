d026af9d2876f451e3d40e99ff60a518a3229c1c
Viet Anh Nguyen <vietanh.dev@gmail.com>
2023-07-13 23:20:30 +0700

    Fix could not build app

--- src/app/[lng]/dashboard/page.js
+++ src/app/[lng]/dashboard/page.js
@@ -13,7 +13,7 @@ import ChartOne from "@/components/dashboard/ChartOne.tsx";
 import ChartThree from "@/components/dashboard/ChartThree.tsx";
 import ChartTwo from "@/components/dashboard/ChartTwo.tsx";
 
-export default function Page({ children, params: { lng } }) {
+export default function Page() {
   return (
     <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
