81942815493e65f51641d30cd36716014b97ff5a
Viet Anh Nguyen <vietanh.dev@gmail.com>
2023-08-15 23:55:08 +0700

    Clean up dashboard

--- src/components/dashboard/CardTwo.tsx
+++ src/components/dashboard/CardTwo.tsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-const CardTwo = ({ totalCustomer }: { totalCustomer: number }) => {
+const CardTwo = ({ totalCustomer }: { totalCustomer: number; lng: string }) => {
   const style = `
     a.number-link {
       color:#155e75;
@@ -7,7 +6,7 @@ const CardTwo = ({ totalCustomer }: { totalCustomer: number }) => {
     .text-timespan {
       white-space: nowrap;
     }
-    
+
 .view-detail-wrapper{
   position: absolute;
   bottom: 0;
@@ -39,29 +38,6 @@ div.view-detail:hover:after{
   width: 100%;
 }
   `;
-
-  const [timespan, setTimespan] = useState("hôm nay"); // State for time span
-  const timespans = ["tuần trước", "hôm qua", "hôm nay", "tuần này"];
-  const [customer, setCustomer] = useState(68); // State for customer
-  const customerList = [361, 52, 68, 532];
-
-  // Function to handle previous time span
-  const handlePrevTimespan = () => {
-    const index = timespans.indexOf(timespan);
-    if (index > 0) {
-      setTimespan(timespans[index - 1]);
-      setCustomer(customerList[index - 1]);
-    }
-  };
-
-  // Function to handle next time span
-  const handleNextTimespan = () => {
-    const index = timespans.indexOf(timespan);
-    if (index < timespans.length - 1) {
-      setTimespan(timespans[index + 1]);
-      setCustomer(customerList[index + 1]);
-    }
-  };
 
   return (
     <div className="relative pb-[3rem] rounded-sm  bg-white py-6 px-6 border border-stroke dark:border-strokedark dark:bg-gray-800 shadow-default">
@@ -101,87 +77,12 @@ div.view-detail:hover:after{
           </span>
         </div>
       </div>
-      <div className="mt-4 flex gap-[0.4rem] items-start justify-between">
-        <div className="flex flex-col ms-1 w-[96px]">
-          <span className="text-md font-bold text-center ms-[-1rem]">
-            +{customer.toLocaleString("vi-VN")}
-          </span>
-          <div className="flex gap-1 items-center justify-center mt-1 ms-[-5px]">
-            <div className="w-[20px] select-none">
-              {timespans.indexOf(timespan) > 0 && (
-                <div
-                  className="clickable flex items-center h-5 bg-cyan-50 p-1 border border-stroke dark:border-strokedark dark:bg-gray-800"
-                  onClick={handlePrevTimespan}
-                >
-                  <svg
-                    fill="#000000"
-                    height="full"
-                    width="full"
-                    version="1.1"
-                    id="XMLID_287_"
-                    viewBox="0 0 24 24"
-                  >
-                    <g id="next">
-                      <g>
-                        <polygon points="17.2,23.7 18.6,22.3 8.3,12 18.6,1.7 17.2,0.3 5.5,12 		" />
-                      </g>
-                    </g>
-                  </svg>
-                </div>
-              )}
-            </div>
-            <span className="text-timespan text-xs select-none">
-              {timespan}
-            </span>
-            <div className="w-[20px]  select-none">
-              {timespans.indexOf(timespan) < timespans.length - 1 && (
-                <div
-                  className="clickable flex items-center h-5 bg-cyan-50 p-1 border border-stroke dark:border-strokedark dark:bg-gray-800"
-                  onClick={handleNextTimespan}
-                >
-                  <svg
-                    fill="#000000"
-                    height="full"
-                    width="full"
-                    version="1.1"
-                    id="XMLID_287_"
-                    viewBox="0 0 24 24"
-                  >
-                    <g id="next">
-                      <g>
-                        <polygon points="6.8,23.7 5.4,22.3 15.7,12 5.4,1.7 6.8,0.3 18.5,12 		" />
-                      </g>
-                    </g>
-                  </svg>
-                </div>
-              )}
-            </div>
-          </div>
-        </div>
-        <div className="self-stretch w-[1px] bg-gray-200"></div>
-        <div
-          className="flex flex-col w-[80px] text-center"
-          title="Retention rate: Tỉ lệ khách hàng quay lại"
-        >
-          <span className="text font-bold text-black dark:text-white select-none">
-            RR*
-          </span>
-          <span className="text-md font-bold ms-1">69%</span>
-        </div>
-        <div className="self-stretch w-[1px] bg-gray-200"></div>
-        <div
-          className="flex flex-col w-[80px] text-center"
-          title="Average order value: Giá trị đơn hàng trung bình"
-        >
-          <span className="text font-bold text-black dark:text-white select-none">
-            AOV*
-          </span>
-          <span className="text-md font-bold ms-1">142,000đ</span>
-        </div>
-      </div>
       <div className="view-detail-wrapper clickable card-head absolute bottom-0 left-0 w-full flex gap-1 justify-center items-center h-[2rem] border-t border-stroke  shadow-default dark:border-strokedark dark:bg-gray-800">
         <div className="view-detail w-full h-full flex">
-          <a className="m-auto z-10 text-cyan-600 select-none hover:underline">
+          <a
+            className="m-auto z-10 text-cyan-600 select-none hover:underline"
+            href={`dashboard/customers-360`}
+          >
             Xem Khách hàng 360°
           </a>
         </div>
